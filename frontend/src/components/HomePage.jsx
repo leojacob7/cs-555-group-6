b4d94b9f32b21cdd50cb6b50f231c84742efa69f
Bhagya I <bingale@stevens.edu>
2022-10-17 19:43:08 -0400

    added top accounts section

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { useState } from 'react'
 import { Posts } from './Posts'
 import moment from 'moment'
-import { Box, Button, Center, Container, Heading, HStack, Icon, Stack, useColorModeValue, VStack } from '@chakra-ui/react'
+import { Box, Button, Center, Container, Heading, HStack, Icon, Stack, useColorModeValue, VStack, Text, Image, Flex, Avatar } from '@chakra-ui/react'
 import Navbar from './navbar';
 import { AtSignIcon  } from '@chakra-ui/icons'
+import { Divider } from '@chakra-ui/react'
+import Profile from './Profile'
 
 export const HomePage = () => {
   const test_posts = [
@@ -17,7 +19,7 @@ export const HomePage = () => {
   return (   
     <div>
       <Navbar />
-      
+      <HStack>
       <Box px={8} py={10}>
       <Stack spacing='20px'>
       <Box p={5} w='800px'
@@ -54,7 +56,77 @@ export const HomePage = () => {
           </Box>
       </Stack>
       </Box>
-      
+      <Box p={10} h={760} w='600px'
+            
+            bg={useColorModeValue('white', 'gray.800')}
+            boxShadow={'2xl'}
+            rounded={'md'}
+            overflow={'hidden'}>
+
+            <VStack>
+              <Box><Text fontSize='lg'>Top Accounts</Text></Box>
+              <Divider />
+              <VStack>
+              <Box
+               maxW={'270px'}
+               maxH={'500px'}
+          w={'280px'}
+          bg={useColorModeValue('white', 'gray.800')}
+          boxShadow={'2xl'}
+          rounded={'md'}
+          overflow={'hidden'}>
+          
+          <Box justify={'center'} mt={-12}>
+            <Avatar
+              size={'md'}
+              src={
+                'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+              }
+              alt={'Author'}
+              css={{
+                border: '2px solid white',
+              }}
+            />
+          </Box>
+
+
+          <Box p={6}>
+            <Stack spacing={0} align={'center'} mb={5}>
+              <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+                John Doe
+              </Heading>
+              <Text color={'gray.500'}>Frontend Developer</Text>
+            </Stack>
+
+            
+
+            <Button
+              w={'full'}
+              mt={8}
+              bg={useColorModeValue('#151f21', 'gray.900')}
+              color={'white'}
+              rounded={'md'}
+              _hover={{
+                transform: 'translateY(-2px)',
+                boxShadow: 'lg',
+              }}>
+              Follow
+            </Button>
+            
+          </Box>
+          
+        </Box>
+             
+        </VStack>
+            </VStack>
+            
+          </Box>
+
+
+
+
+
+      </HStack>  
     </div>
 
   )
