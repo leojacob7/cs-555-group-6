8b37e92136fa66a2d7a82fbfb58e0f961c2bf765
Bhagya I <bingale@stevens.edu>
2022-10-17 19:03:13 -0400

    added hot and trending bar and aligned posts

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { useState } from 'react'
 import { Posts } from './Posts'
 import moment from 'moment'
-import { Container, Heading } from '@chakra-ui/react'
+import { Box, Button, Center, Container, Heading, HStack, Icon, Stack, useColorModeValue, VStack } from '@chakra-ui/react'
 import Navbar from './navbar';
+import { AtSignIcon  } from '@chakra-ui/icons'
 
 export const HomePage = () => {
   const test_posts = [
@@ -13,13 +14,48 @@ export const HomePage = () => {
     { id: 4, title: "How was your day?", posted: moment(Date.now()), user: { id: 1, firstName: "John", lastName: "Doe", username: "jdoe" } }]
 
   const [posts, setPosts] = useState([...test_posts])
-  return (
+  return (   
     <div>
       <Navbar />
-      <Container width='full'>
-        <Heading as='h1' size='2xl' my='5'>Home</Heading>
-        <Posts posts={posts} />
-      </Container>
+      
+      <Box px={8} py={10}>
+      <Stack spacing='20px'>
+      <Box p={5} w='800px'
+            
+            bg={useColorModeValue('white', 'gray.800')}
+            boxShadow={'2xl'}
+            rounded={'md'}
+            overflow={'hidden'}>
+     
+            <HStack spacing='20px'> 
+              <Box py={'-90px'}>
+              <Button leftIcon={<AtSignIcon />} colorScheme='blue' size='md' variant='outline'>Hot Posts</Button> 
+              </Box>
+
+              <Box>
+              <Button leftIcon={<AtSignIcon />}  colorScheme='blue' size='md' variant='outline'>New</Button>
+              </Box>
+
+              <Box>
+              <Button leftIcon={<AtSignIcon />}  colorScheme='blue' size='md' variant='outline'>Trending</Button>
+              </Box>
+              
+
+            </HStack>
+
+      </Box>
+      <Box p={10} w='800px'
+            
+            bg={useColorModeValue('white', 'gray.800')}
+            boxShadow={'2xl'}
+            rounded={'md'}
+            overflow={'hidden'}>
+            <Posts posts={posts} />
+          </Box>
+      </Stack>
+      </Box>
+      
     </div>
+
   )
 }
