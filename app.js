85aa48a37ffe0e477c8e0415d394288a4f7406d1
unknown <ssrimant@stevens.edu>
2022-10-21 17:58:38 -0400

    Add likes backend - resoving confilcts

--- app.js
+++ app.js
@@ -8,30 +8,42 @@ const bodyParser = require('body-parser');
 
 app.use('/public', static);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(express.urlencoded({ extended: true }));
 
+app.use(
+	session({
+		name: 'AuthCookie',
+		secret: 'some secret string!',
+		resave: false,
+		saveUninitialized: true,
+	})
+);
 
-
-app.use(session({
-  name: 'AuthCookies',
-  secret: 'some secret string!!',
-  resave: false,
-  saveUninitialized: true
-}))
-
-
-
-  app.use(async (req, res, next) => { 
-    console.log("req.session.user : "+req.session.user)
-    if(req.session.user){ 
-console.log("["+new Date().toUTCString()+"]: "+req.method+" "+req.originalUrl+ ' (Authenticated User)');
-    }
-    else{
-      console.log("["+new Date().toUTCString()+"]: "+req.method+" "+req.originalUrl+ ' (Non-Authenticated User)');
-    }
-next();
-  });
+app.use(async (req, res, next) => {
+	if (req.session.user) {
+		console.log(
+			'[' +
+				new Date().toUTCString() +
+				']: ' +
+				req.method +
+				' ' +
+				req.originalUrl +
+				' (Authenticated User)'
+		);
+	} else {
+		console.log(
+			'[' +
+				new Date().toUTCString() +
+				']: ' +
+				req.method +
+				' ' +
+				req.originalUrl +
+				' (Non-Authenticated User)'
+		);
+	}
+	next();
+});
 
   configRoutes(app);
 
--- routes/users.js
+++ routes/users.js
@@ -24,18 +24,15 @@ router.route('/login')
             const usrLoginChkResponse = await userData.loginCheck(userName,passwd);
             //console.log("usrLoginChkResponse : "+usrLoginChkResponse);
 
-            if(usrLoginChkResponse === "{authenticated: true}"){
-                request.session.user = {username: userName};
-                
-                response.status(200).send("User Succesfully logged in");
+            if (usrLoginChkResponse === '{authenticated: true}') {
+                request.session.user = { username: userName };
+                response.status(200).json('User Succesfully logged in');
             }
-                      
-        }
-        catch(e){
-            response.status(400).send("Error logging in : "+e);
+        } catch (e) {
+            response.status(400).json('Error logging in : ' + e);
             return;
         }
-       });
+    });
 
 
        router.route('/logout')
