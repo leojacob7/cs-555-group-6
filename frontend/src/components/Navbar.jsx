15568dcdb818810609c4efd2f9dcfa9d56f91793
Leo B Jacob <leobjacob@gmail.com>
2022-12-02 12:08:19 -0500

    Refactor top accounts

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,118 +1,116 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import {
-	Box,
-	Avatar,
-	Button,
-	Menu,
-	MenuList,
-	MenuItem,
-	Divider,
-	Typography,
-	Sheet,
-} from '@mui/joy';
-import { Stack } from '@mui/material';
-import Link from '@mui/joy/Link';
-import { Link as RouterLink } from 'react-router-dom';
+  Box,
+  Avatar,
+  Button,
+  Menu,
+  MenuList,
+  MenuItem,
+  Divider,
+  Typography,
+  Sheet
+} from '@mui/joy'
+import { Stack } from '@mui/material'
+import Link from '@mui/joy/Link'
+import { Link as RouterLink } from 'react-router-dom'
 // import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 // import AddNewPost from './AddNewPost';
-import { useAuth } from '../context/userContext';
-import { useNavigate } from 'react-router-dom';
-import { useAxios } from '../utils/apiCalls';
+import { useAuth } from '../context/userContext'
+import { useNavigate } from 'react-router-dom'
+import { useAxios } from '../utils/apiCalls'
 
-export default function Navbar() {
-	// for menu list
-	const [anchorEl, setAnchorEl] = useState(null);
-	// const [, error, setError, loading, operation] = useAxios()
-	const navigate = useNavigate();
-	const { setSession, setUser, user } = useAuth();
-	const open = Boolean(anchorEl);
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+export default function Navbar () {
+  // for menu list
+  const [anchorEl, setAnchorEl] = useState(null)
+  // const [, error, setError, loading, operation] = useAxios()
+  const navigate = useNavigate()
+  const { setSession, setUser, user } = useAuth()
+  const open = Boolean(anchorEl)
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
 
-	const handleLogout = () => {
-		// operation('logout')
-		setAnchorEl(null);
-		setSession(null);
-		setUser(null);
-		navigate('/login');
-	};
+  const handleLogout = () => {
+    // operation('logout')
+    setAnchorEl(null)
+    setSession(null)
+    setUser(null)
+    navigate('/login')
+  }
 
-	const handleProfile = () => {
-		setAnchorEl(null);
-		navigate(`/profile/${user._id}`);
-	};
+  const handleProfile = () => {
+    setAnchorEl(null)
+    navigate(`/profile/${user._id}`)
+  }
 
-	const handleEditProfile = () => {
-		setAnchorEl(null);
-		navigate('/update-profile');
-	};
+  const handleEditProfile = () => {
+    setAnchorEl(null)
+    navigate('/update-profile')
+  }
 
-	return (
-		<Sheet variant='soft' color='neutral' sx={{ paddingX: 4, paddingY: 3 }}>
-			<Stack
-				maxHeight={20}
-				direction='row'
-				alignItems='center'
-				justifyContent='space-between'>
-				<Box>
-					<Link component={RouterLink} level='body1' to='/'>
-						Logo
-					</Link>
-				</Box>
+  return (
+    <Sheet variant='soft' color='neutral' sx={{ paddingX: 4, paddingY: 3 }}>
+      <Stack
+        maxHeight={20}
+        direction='row'
+        alignItems='center'
+        justifyContent='space-between'
+      >
+        <Box>
+          <Link component={RouterLink} level='body1' to='/'>
+            Logo
+          </Link>
+        </Box>
 
-				<Stack direction='row' spacing={2}>
-					{/* <AddNewPost /> */}
-					{/* <Button onClick={toggleColorMode}>
+        <Box>
+          <Link component={RouterLink} level='body1' to='/'>
+            Home Page
+          </Link>
+        </Box>
+
+        <Stack direction='row' spacing={2}>
+          {/* <AddNewPost /> */}
+          {/* <Button onClick={toggleColorMode}>
 								{colorMode === 'light' ? (
 									<MoonIcon />
 								) : (
 									<SunIcon />
 								)}
 							</Button> */}
-					<Button
-						variant='outlined'
-						cursor='pointer'
-						onClick={handleClick}>
-						<Avatar
-							size='sm'
-							src={
-								user?.avatar ||
-								'https://avatars.dicebear.com/api/male/username.svg'
-							}
-						/>
-					</Button>
-					<Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
-						<Stack direction='column' p={2} spacing={2}>
-							<Avatar
-								size='xl'
-								src={
-									user?.avatar ||
-									'https://avatars.dicebear.com/api/male/username.svg'
-								}
-							/>
+          <Button variant='outlined' cursor='pointer' onClick={handleClick}>
+            <Avatar
+              size='sm'
+              src={
+                user?.avatar ||
+                'https://avatars.dicebear.com/api/male/username.svg'
+              }
+            />
+          </Button>
+          <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+            <Stack direction='column' p={2} spacing={2}>
+              <Avatar
+                size='xl'
+                src={
+                  user?.avatar ||
+                  'https://avatars.dicebear.com/api/male/username.svg'
+                }
+              />
 
-							<Typography level='body1' component='strong'>
-								{`Hello ${user?.firstName}`}
-							</Typography>
-							<MenuList alignItems='center'>
-								<MenuItem onClick={handleProfile}>
-									Your Profile
-								</MenuItem>
-								<MenuItem onClick={handleEditProfile}>
-									Update profile
-								</MenuItem>
-								<MenuItem onClick={handleLogout}>
-									Logout
-								</MenuItem>
-							</MenuList>
-						</Stack>
-					</Menu>
-				</Stack>
-			</Stack>
-		</Sheet>
-	);
+              <Typography level='body1' component='strong'>
+                {`Hello ${user?.firstName}`}
+              </Typography>
+              <MenuList alignItems='center'>
+                <MenuItem onClick={handleProfile}>Your Profile</MenuItem>
+                <MenuItem onClick={handleEditProfile}>Update profile</MenuItem>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </MenuList>
+            </Stack>
+          </Menu>
+        </Stack>
+      </Stack>
+    </Sheet>
+  )
 }
--- frontend/src/components/TopAccounts.jsx
+++ frontend/src/components/TopAccounts.jsx
@@ -1,61 +1,58 @@
-import { Avatar, Box, Divider, Stack, Typography } from '@mui/joy';
-import React, { useEffect } from 'react';
-import { useAxios } from '../utils/apiCalls';
+import { Box, Divider, Stack, Typography } from '@mui/joy'
+import React, { useEffect } from 'react'
+import { useAxios } from '../utils/apiCalls'
+import Avatar from 'react-avatar'
+import { Link } from 'react-router-dom'
 
 export const TopAccounts = () => {
-	const { data, setLoading, loading, operation } = useAxios();
+  const { data, setLoading, loading, operation } = useAxios()
 
-	const fetchTopUsers = async () => {
-		await operation('top-accounts', null, 'GET');
-	};
+  const fetchTopUsers = async () => {
+    await operation('top-accounts', null, 'GET')
+  }
 
-	console.log('top users', typeof data);
+  console.log('top users', data)
 
-	useEffect(() => {
-		fetchTopUsers();
-	}, []);
-	return (
-		<Stack
-			direction='column'
-			alignItems='flex-start'
-			maxHeight='100vh'
-			spacing={3}>
-			<Box>
-				<Typography level='h5'>Top Accounts</Typography>
-				<Divider />
-			</Box>
+  useEffect(() => {
+    fetchTopUsers()
+  }, [])
+  if (data === '') return <div>Test</div>
+  return (
+    <Stack direction='column' alignItems='center' maxHeight='100vh' spacing={3}>
+      <Box sx={{ display: 'flex', alignItems: 'center' }}>
+        <Typography level='h5'>Top Accounts</Typography>
+        <Divider />
+      </Box>
 
-			{data.map((user) => (
-				<Stack spacing={1}>
-					<Stack alignItems='center' justifyContent='center'>
-						<Avatar
-							sx={{ height: '75px', width: '75px' }}
-							src={
-								'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
-							}
-							alt={'Author'}
-							css={{
-								border: '2px solid white',
-							}}
-						/>
-					</Stack>
+      {data.users.map(user => (
+        <Link to={`/profile/${user._id}`}>
+          <Stack spacing={1}>
+            <Stack alignItems='center' justifyContent='center'>
+              <Avatar
+                sx={{ height: '75px', width: '75px' }}
+                name={`${user.firstName} ${user.lastName}`}
+                alt={'Author'}
+                css={{
+                  border: '2px solid white'
+                }}
+                round={true}
+              />
+            </Stack>
 
-					<Stack spacing={2}>
-						<Stack align='flex-start'>
-							<Typography level='body1' component='strong'>
-								John Doe
-							</Typography>
-							<Typography level='body2'>
-								Frontend Developer
-							</Typography>
-						</Stack>
+            <Stack spacing={2}>
+              <Stack align='flex-start'>
+                <Typography level='body1' component='strong'>
+                  {user.firstName}
+                </Typography>
+              </Stack>
 
-						{/* <Button variant='soft' size='sm'>
+              {/* <Button variant='soft' size='sm'>
 									Follow
 								</Button> */}
-					</Stack>
-				</Stack>
-			))}
-		</Stack>
-	);
-};
+            </Stack>
+          </Stack>
+        </Link>
+      ))}
+    </Stack>
+  )
+}
